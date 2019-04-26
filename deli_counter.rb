15976f3fda64d3157f5c41026e280ca1e474fbae
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 04:51:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(names)
+  puts "The line is currently empty"
+end
+
+def take_a_number(katz_deli, name)
+  #the person's name along with their position in line
+  puts
+  
+def now_serving
+end
