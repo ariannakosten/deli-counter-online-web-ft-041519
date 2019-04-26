456e0f900558e73ac771f2647858d35c76ad4f7c
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 05:28:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ end
 def now_serving(array)
   if array.empty? == true 
     puts "There is nobody waiting to be served!"
-  elsif array.empty? == false 
+    elsif array.empty? == false 
     puts "Currently serving #{array.shift}."
   end 
 end 
