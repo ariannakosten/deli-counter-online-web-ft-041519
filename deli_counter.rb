91c434abfc1bfa42e159369f192ae7950e5df507
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 04:57:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,7 @@ end
 def take_a_number(katz_deli, name)
   #the array for the current line of people (katz_deli), and a string containing the name of the person joining the end of the line. puts the person's name along with their position in line
   puts
+end
   
 def now_serving
   #puts the next person in line and then remove them from the front. If there is nobody in line, it should call out (puts) that "There is nobody waiting to be served!"
