3416fd2adce858faad77013646b3992754284ef6
Arianna Kosten <arianna.kosten@gmail.com>
2019-04-26 05:24:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,7 @@ def take_a_number(katz_deli, name)
 end
   
   
-def now_serving
-  #puts the next person in line and then remove them from the front. If there is nobody in line, it should call out (puts) that "There is nobody waiting to be served!"
+#puts the next person in line and then remove them from the front. If there is nobody in line, it should call out (puts) that "There is nobody waiting to be served!"
+
+
 end
